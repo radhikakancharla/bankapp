e444c14c05315466e2a99e6e00b76dd744825d17
Radhika Kancharla <radhika.kancharla@mckesson.com>
2017-08-11 16:08:29 +0000

    changes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,35 @@
 class AccountsController < ApplicationController
     def index
         @accounts = Account.order(id: :desc)
-        
+    end
+    def bankapp
+        @account = Account.find(params[:id])
+    end
+    def deposit
+        @account= Account.find(params[:id])
+        @account.deposit(params[:amount].to_f)
+        if @account.errors.any?
+            render 'bankapp'
+        else
+            redirect_to root_url
+        end
+    end
+    def withdraw
+        @account= Account.find(params[:id])
+        @account.withdraw(params[:amount].to_f)
+        if @account.errors.any?
+            render 'bankapp'
+        else
+            redirect_to root_url
+        end
+    end
+    def clearsuspension
+        @account= Account.find(params[:id])
+        @account.clear_suspension
+        if @account.errors.any?
+            render 'bankapp'
+        else
+            redirect_to root_url
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'accounts#index'
-  resources :accounts, only:[:index]
+  resources :accounts, only:[:index] do
+    member do
+      get '/bankapp', to:'accounts#bankapp'
+      post '/deposit', to: 'accounts#deposit'
+      post '/withdraw', to: 'accounts#withdraw'
+      post '/clearsuspension', to: 'accounts#clear_suspension'
+    end
+  end
 end
